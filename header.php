2501f353f7ff9e9701320aceabf7e44b90bae697
dseixas <dseixas247@gmail.com>
2024-11-25 14:46:48 +0000

    Criação do ficheiro header e footer

---
+++ header.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Craigslist</title>
+
+</head>
+
+<body>
+
+    <div id="menu">
+
+        <ul>
+
+        </ul>
+
+    </div>
