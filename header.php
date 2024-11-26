1e3cd9356aa7868de91aa46e2ab4040ea3ca364d
dseixas <dseixas247@gmail.com>
2024-11-26 14:38:21 +0000

    css applied

--- header.php
+++ header.php
@@ -10,13 +10,16 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/buttons.css">
+
     <title>Craigslist</title>
 
 </head>
 
 <body>
 
-    <div id="menu">
+    <div id="navbar">
 
         <ul>
 
