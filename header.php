a1e425dce45253fb9bae8e66c1e09084c7d3c619
dseixas <dseixas247@gmail.com>
2024-12-05 19:19:47 +0000

    Register page created

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/register.css">
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/buttons.css">
     <link rel="stylesheet" href="css/searchbar.css">
